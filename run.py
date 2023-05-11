1f2691e806838eb0ef5769d121ca0bb6ad85a87e
362227 <91358231+362227@users.noreply.github.com>
2023-05-11 11:59:42 +0800

    Update run.py

--- run.py
+++ run.py
@@ -6,13 +6,13 @@ time.sleep(2)
 
 
 #抓supjav
-os.system('python uc.py https://supjav.com/category/amateur > supjav.html')
-os.system('python uc.py https://supjav.com/category/censored-jav >> supjav.html')
-os.system('python uc.py https://supjav.com/category/censored-jav/page/2 >> supjav.html')
-os.system('python uc.py https://supjav.com/category/censored-jav/page/3 >> supjav.html')
-os.system('python uc.py https://supjav.com/category/censored-jav/page/4 >> supjav.html')
-os.system('python uc.py https://supjav.com/category/censored-jav/page/5 >> supjav.html')
-os.system('python uc.py https://supjav.com/category/censored-jav/page/6 >> supjav.html')
+os.system('rm -rf javgg-amateur.html; python uc.py https://supjav.com/category/amateur > supjav-amateur.html')
+os.system('rm -rf supjav-censored.html; python uc.py https://supjav.com/category/censored-jav > supjav-censored.html')
+os.system('rm -rf supjav-censored-2.html; python uc.py https://supjav.com/category/censored-jav/page/2 > supjav-censored-2.html')
+os.system('rm -rf supjav-censored-3.html; python uc.py https://supjav.com/category/censored-jav/page/3 > supjav-censored-3.html')
+os.system('rm -rf supjav-censored-4.html; python uc.py https://supjav.com/category/censored-jav/page/4 > supjav-censored-4.html')
+os.system('rm -rf supjav-censored-5.html; python uc.py https://supjav.com/category/censored-jav/page/5 > supjav-censored-5.html')
+os.system('rm -rf supjav-censored-6.html; python uc.py https://supjav.com/category/censored-jav/page/6 > supjav-censored-6.html')
 
 
 #抓javgg
