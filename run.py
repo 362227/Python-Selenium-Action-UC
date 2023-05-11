e00237d3395a7e878c24f1fa23c82426d4a67a9b
362227 <91358231+362227@users.noreply.github.com>
2023-05-11 11:08:01 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,20 @@
 import os
 import sys
 
+#抓javgg
 os.system('python uc.py https://javgg.net/new-post > javgg.html')
+
+#抓supjav
+os.system('python uc.py https://supjav.com/category/amateur > supjav.html')
+os.system('python uc.py https://supjav.com/category/censored-jav >> supjav.html')
+os.system('python uc.py https://supjav.com/category/censored-jav/page/2 >> supjav.html')
+os.system('python uc.py https://supjav.com/category/censored-jav/page/3 >> supjav.html')
+os.system('python uc.py https://supjav.com/category/censored-jav/page/4 >> supjav.html')
+os.system('python uc.py https://supjav.com/category/censored-jav/page/5 >> supjav.html')
+os.system('python uc.py https://supjav.com/category/censored-jav/page/6 >> supjav.html')
+
+
+
+
+
+
